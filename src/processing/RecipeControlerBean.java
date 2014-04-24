ca0bc2038adbf3bce6ebb6b2b99c083a6db72672
keload <keload@gmail.com>
2014-04-24 16:50:21 +0200

    add processing/RecipeControlerBean.java and processing/UserControlerBean.java

---
+++ src/processing/RecipeControlerBean.java
@@ -0,0 +1,45 @@
+package processing;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import model.RecipeListModelBean;
+import model.RecipeModel;
+import dao.DaoFabric;
+import dao.RecipesDao;
+
+@ManagedBean
+@ApplicationScoped
+
+public class RecipeControlerBean {
+	private RecipesDao recipeDao;
+	
+	public RecipeControlerBean() {
+		this.recipeDao=DaoFabric.getInstance().createRecipesDao();
+	}
+	
+	
+	public void loadAllRecipe(){
+		ArrayList<RecipeModel> list = this.recipeDao.getAllRecipes();
+		
+		RecipeListModelBean recipeList=new RecipeListModelBean();
+		
+		for(RecipeModel recipe:list){
+			recipeList.addRecipeList(recipe);
+		}
+		
+		//récupère l'espace de mémoire de JSF
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		Map<String, Object> sessionMap = externalContext.getSessionMap();
+		
+		//place la liste de recette dans l'espace de mémoire de JSF
+		sessionMap.put("recipeList", recipeList);
+		
+	}
+
+}
